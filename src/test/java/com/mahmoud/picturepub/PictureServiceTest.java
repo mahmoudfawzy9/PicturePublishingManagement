d2a93b376ee2926a124fbaab373aa3ded0477097
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-04-08 22:57:18 +0200

    Update PictureServiceTest.java

--- src/test/java/com/mahmoud/picturepub/PictureServiceTest.java
+++ src/test/java/com/mahmoud/picturepub/PictureServiceTest.java
@@ -11,18 +11,11 @@ import com.mahmoud.picturepub.entity.Status;
 import com.mahmoud.picturepub.service.PictureServiceImp;
 import org.json.JSONException;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
